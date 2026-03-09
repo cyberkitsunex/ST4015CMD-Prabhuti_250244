6df6e2f9be0f92d7380942558ae12bb2a23186bf
cyberkitsunex <cyberkitsunex@gmail.com>
2026-03-09 14:36:31 +0545

    managed folder

---
+++ Task1_Encoding_Security/encoding_demo.py
@@ -0,0 +1,24 @@
+import base64
+from urllib.parse import quote, unquote
+
+# Base64
+encoded = base64.b64encode(text.encode()).decode()
+decoded = base64.b64decode(encoded).decode()
+print(f"\nOriginal : {text}")
+print(f"Encoded Base64 : {encoded}")
+print(f"Decoded  : {decoded}")
+
+# ASCII
+text = "This is Encoding"
+print("This is ASCII Value")
+for ch in text:
+    
+    print(f"{ch} => {ord(ch)}")
+
+# URL Encoding
+url = "name=Sanjok Rai&city=Kathmandu Nepal"
+enc_url = quote(url)
+dec_url = unquote(enc_url)
+print(f"\nOriginal : {url}")
+print(f"Encoded URL : {enc_url}")
+print(f"Decoded  : {dec_url}")
